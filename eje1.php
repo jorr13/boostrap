e0cd0cd6b4e072d1193ae4cd64bd1f013a51fa67
jorr13 <jerinson1996@gmail.com>
2019-04-22 06:32:31 -0400

    practica de boostrap

---
+++ eje1.php
@@ -0,0 +1,18 @@
+<?php
+
+$nota1 = 10;
+$nota2= 7;
+$nota3 = 10;
+
+
+$nota_final = ($nota1 + $nota2 + $nota3)/3;
+
+ if($nota_final >= 70){
+    echo 'felicidades usted esta aprovado, su nota es: '. $nota_final;
+ }else{
+    echo 'que chimbo mano no pasas, tu nota es: '. $nota_final;
+ }
+
+
+
+?>
